fc9cabab0dcfaa2c50300aba8637499d1c1e2258
Erik Gustavsson <erik@stickybeat.se>
2014-09-25 09:14:49 +0200

    Basic stuff

---
+++ 1.0/WorkingClass.js
@@ -0,0 +1,36 @@
+
+WorkingClass = {
+
+	initBindsAll: true,
+	initSynthesizes: true,
+
+	init: function( obj ){
+
+		if( WorkingClass.initBindsAll ) WorkingClass.bindAll( obj );
+		if( WorkingClass.initSynthesizes ) WorkingClass.synthesize( obj );
+	},
+
+	bindAll: function( obj ){
+
+		for( var a in obj ){
+			var b = obj[ a ];
+			if( typeof( b ) == 'function' ){
+				obj[a]=b.bind(obj);
+			}
+		}
+	},
+
+	synthesize: function( obj ){
+
+		for( var a in obj ){
+			var b = obj[ a ];
+			if( typeof( b ) != 'function' ){
+				var c = a.substr(0,1).toUpperCase() + a.substr(1);
+				var g = 'get'+c;
+				if( !obj[g] ) obj[g]=function(){ return this.a[ this.b ] }.bind({a:obj,b:a});
+				var s = 'set'+c;
+				if( !obj[s] ) obj[s]=function(v){ return this.a[ this.b ] = v; }.bind({a:obj,b:a});
+			}
+		}
+	}
+};
